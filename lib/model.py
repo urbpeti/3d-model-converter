fbc72e2631f3b8bbad06b81cefc660bfa0a6dc1e
Peter <urbpeti@gmail.com>
2019-06-08 13:00:39 +0200

    Model get trianlge returns simple triangles

--- lib/model.py
+++ lib/model.py
@@ -4,4 +4,13 @@ class Model:
         self._faces = faces
 
     def get_triangles(self):
-        pass
+        return [self._get_triangles_from_face(face) for face in self._faces]
+
+    def _get_face_with_vectors(self, face):
+        return [self._vectors[i - 1] for i in face]
+
+    def _get_triangles_from_face(self, face):
+        normal = [0.0] * 3
+        triangle = self._get_face_with_vectors(face)
+        triangle.insert(0, normal)
+        return triangle
---
+++ lib/model_test.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from lib.model import Model
+
+
+class ModelTest(TestCase):
+    def test_get_triangles_return_trianlge_when_it_is_one_triangle(self):
+        model = Model(
+            vectors=[
+                [0.0, 2.0, 2.0],
+                [0.0, 0.0, 0.0],
+                [2.0, 0.0, 0.0],
+            ],
+            faces=[
+                [1, 2, 3],
+                [2, 1, 3],
+            ]
+        )
+
+        expected_triangles = [
+            [
+                [0.0, 0.0, 0.0],
+                [0.0, 2.0, 2.0],
+                [0.0, 0.0, 0.0],
+                [2.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0],
+                [0.0, 2.0, 2.0],
+                [2.0, 0.0, 0.0],
+            ]
+        ]
+
+        triangles = model.get_triangles()
+
+        self.assertEqual(str(triangles), str(expected_triangles))
